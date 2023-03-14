617dd911133b401e9338fbabffe76fff9dded7ab
Lê Đăng Quang <81614913+ldqgithub12@users.noreply.github.com>
2023-03-14 12:34:08 +0700

    kkk

--- Day_13/demo_hook/src/Pages/user/UserDetails.jsx
+++ Day_13/demo_hook/src/Pages/user/UserDetails.jsx
@@ -1,67 +1,124 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from 'axios';
+
 
 function UserDetails() {
   let { userId } = useParams();
+  const [user, setUser] = useState(null);
+  const [provinces, setProvinces] = useState([]);
+  const ENDPOINT = "http://localhost:8080/api/users";
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        let rs = await axios.get(`${ENDPOINT}/${userId}`)
+        setUser(rs.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchUser();
+  }, [])
+  useEffect(() => {
+    const fetchProvince = async () => {
+      try {
+        let rs = await axios.get("https://provinces.open-api.vn/api/p/")
+        setProvinces(rs.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchProvince();
+  }, [])
+  const handleChangeAvatar = async (e) => {
+    const file = e.target.files[0]
+    const formData = new FormData();
+    formData.append("file", file);
+    try {
+      const rs = await axios.put(`http://localhost:8080/api/users/${userId}/update-avatar`, formData, {
+        headers: {
+          "Content-Type": "form-data"
+        }
+      });
+      console.log(rs)
+      setUser({ ...user, avatar: rs.data.url })
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+  const handleUpdateUser = async () => {
+    try {
+      let rs = await axios.put(`${ENDPOINT}/${userId}`, user)
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
   return (
-    <div class="container mt-5 mb-5">
-      <h2 class="text-center text-uppercase mb-3">Thông tin user</h2>
-
-      <div class="row justify-content-center">
-        <div class="col-md-6">
-          <div class="bg-light p-4">
-            <div class="mb-3">
-              <label class="col-form-label">Fullname</label>
-              <input type="text" id="fullname" class="form-control" />
+    <div className="container mt-5 mb-5">
+      <h2 className="text-center text-uppercase mb-3">Thông tin user</h2>
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="bg-light p-4">
+            <div className="mb-3">
+              <label className="col-form-label">Fullname</label>
+              <input type="text" id="fullname" className="form-control" value={user?.name} onChange={(e) => setUser({ ...user, name: e.target.value })} />
             </div>
-            <div class="mb-3">
-              <label class="col-form-label">Email</label>
-              <input type="text" id="email" class="form-control" disabled />
+            <div className="mb-3">
+              <label className="col-form-label">Email</label>
+              <input type="text" id="email" className="form-control" disabled value={user?.email} onChange={(e) => setUser({ ...user, email: e.target.value })} />
             </div>
-            <div class="mb-3">
-              <label class="col-form-label">Phone</label>
-              <input type="text" id="phone" class="form-control" />
+            <div className="mb-3">
+              <label className="col-form-label">Phone</label>
+              <input type="text" id="phone" className="form-control" value={user?.phone} onChange={(e) => setUser({ ...user, phone: e.target.value })} />
             </div>
-            <div class="mb-3">
-              <label class="col-form-label">Address</label>
-              <select class="form-select" id="address"></select>
+            <div className="mb-3">
+              <label className="col-form-label">Address</label>
+              <select className="form-select" id="address" value={user?.address} onChange={(e) => setUser({ ...user, address: e.target.value })}>
+                {provinces.map((p) => (
+                  <option key={p.code} value={p.name}>
+                    {p.name}
+                  </option>
+                ))}
+              </select>
             </div>
-            <div class="mb-3">
-              <label class="form-label">Avatar</label>
-              <div class="avatar-preview mb-3 rounded">
+            <div className="mb-3">
+              <label className="form-label">Avatar</label>
+              <div className="avatar-preview mb-3 rounded">
                 <img
-                  src="https://via.placeholder.com/200"
+                  src={`http://localhost:8080${user?.avatar}` ?? "https://via.placeholder.com/200"}
                   alt="avatar"
                   id="avatar-preview"
-                  class="rounded"
+                  className="rounded"
                 />
               </div>
 
-              <label class="btn btn-warning" for="input">
+              <label className="btn btn-warning" for="input">
                 Chọn ảnh
               </label>
-              <input type="file" id="input" class="d-none" />
+              <input type="file" id="input" className="d-none" onChange={(e) => handleChangeAvatar(e)} />
             </div>
-            <div class="mb-3">
-              <label class="col-form-label">Password</label>
-              <div class="">
+            <div className="mb-3">
+              <label className="col-form-label">Password</label>
+              <div className="">
                 <button
                   type="button"
-                  class="btn btn-primary"
+                  className="btn btn-primary"
                   data-bs-toggle="modal"
                   data-bs-target="#modal-change-password"
                 >
                   Đổi mật khẩu
                 </button>
-                <button class="btn btn-warning" id="btn-forgot-password">
+                <button className="btn btn-warning" id="btn-forgot-password">
                   Quên mật khẩu
                 </button>
               </div>
             </div>
           </div>
-          <div class="text-center mt-3">
-            <button class="btn btn-secondary btn-back">Quay lại</button>
-            <button class="btn btn-success" id="btn-save">
+          <div className="text-center mt-3">
+            <button className="btn btn-secondary btn-back">Quay lại</button>
+            <button className="btn btn-success" id="btn-save" onClick={() => handleUpdateUser()}>
               Cập nhật
             </button>
           </div>
@@ -69,7 +126,7 @@ function UserDetails() {
       </div>
 
       <div
-        class="modal fade"
+        className="modal fade"
         id="modal-change-password"
         data-bs-backdrop="static"
         data-bs-keyboard="false"
@@ -77,40 +134,40 @@ function UserDetails() {
         aria-labelledby="staticBackdropLabel"
         aria-hidden="true"
       >
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="staticBackdropLabel">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="staticBackdropLabel">
                 Đổi mật khẩu
               </h5>
               <button
                 type="button"
-                class="btn-close"
+                className="btn-close"
                 data-bs-dismiss="modal"
                 aria-label="Close"
               ></button>
             </div>
-            <div class="modal-body">
-              <div class="mb-3">
-                <label class="col-form-label">Mật khẩu cũ</label>
-                <input type="text" id="old-password" class="form-control" />
+            <div className="modal-body">
+              <div className="mb-3">
+                <label className="col-form-label">Mật khẩu cũ</label>
+                <input type="text" id="old-password" className="form-control" />
               </div>
-              <div class="mb-3">
-                <label class="col-form-label">Mật khẩu mới</label>
-                <input type="text" id="new-password" class="form-control" />
+              <div className="mb-3">
+                <label className="col-form-label">Mật khẩu mới</label>
+                <input type="text" id="new-password" className="form-control" />
               </div>
             </div>
-            <div class="modal-footer">
+            <div className="modal-footer">
               <button
                 type="button"
-                class="btn btn-secondary"
+                className="btn btn-secondary"
                 data-bs-dismiss="modal"
               >
                 Đóng
               </button>
               <button
                 type="button"
-                class="btn btn-primary"
+                className="btn btn-primary"
                 id="btn-change-password"
               >
                 Xác nhận
